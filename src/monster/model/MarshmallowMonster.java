7f5483869a389674747e180e5ef93881f6946ceb
Sharp <rsha7868@740s-pl-127.local>
2017-09-15 12:12:19 -0600

    Detail of Data of the monster.

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -2,5 +2,33 @@ package monster.model;
 
 public class MarshmallowMonster
 {
-
+	//Declaration section of data members
+	
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	
+	
+	public MarshmallowMonster()
+	{
+		//initializes everything to 0, false or null.
+	}
+	public MarshmallowMonster(String name, int eyeCount, int arms, int tentacleCount, boolean hasBloop)
+	{
+		this.name = name;
+		this.tentacleAmount = tentacleCount;
+		this.eyeCount = eyeCount;
+		this.armCount = arms;
+		this.hasBloop = hasBloop;
 }
+	public String toString()
+	{
+		String description = "I am a silly monster my name is " + name + ",I have " + eyeCount + "eyes and only" ;
+		description += tentacleAmount + " tentacles but I have " + armCount + "arms!" + hasBloop + " tells you";
+		description += " if I have a bloop";
+		return description;
+	}
+}
+	}
